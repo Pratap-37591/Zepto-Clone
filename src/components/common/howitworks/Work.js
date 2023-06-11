6833953fbffe6996c3fec9d4f65754b90c5e5646
Pratap-37591 <prathaphb1310@gmail.com>
2023-06-11 22:19:16 +0530

    how it works card file added

--- src/components/common/howitworks/Work.js
+++ src/components/common/howitworks/Work.js
@@ -1,9 +1,46 @@
-import React from 'react'
-
+import { Typography, Box } from "@mui/material";
+import React from "react";
+import "../howitworks/work.css";
 const Work = () => {
   return (
-    <div></div>
-  )
-}
+    <div>
+      <Typography variant="h5" sx={{ textAlign: "center", marginTop: 2 }}>
+        How it works
+      </Typography>
+      <Box className="work_container">
+        <Box className="card-container">
+          <img
+            src="https://cdn.zeptonow.com/mweb-prod/images/pdp/place-order.svg"
+            alt="phone"
+          />
+          <Box>
+            <p>Place an order</p>
+            <p>Choose from a wide range of daily essentials</p>
+          </Box>
+        </Box>
+        <Box className="card-container">
+          <img
+            src="https://cdn.zeptonow.com/mweb-prod/images/pdp/do-not-blink.svg"
+            alt="blink"
+          />
+          <Box>
+            <p>Don't Blink</p>
+            <p>Our delivery parter will be at your door</p>
+          </Box>
+        </Box>
+        <Box className="card-container">
+          <img
+            src="https://cdn.zeptonow.com/mweb-prod/images/pdp/enjoy.svg"
+            alt="enjoy"
+          />
+          <Box>
+            <p>Enjoy</p>
+            <p>Boom! You'll never have to wait for groceries again</p>
+          </Box>
+        </Box>
+      </Box>
+    </div>
+  );
+};
 
-export default Work
+export default Work;
